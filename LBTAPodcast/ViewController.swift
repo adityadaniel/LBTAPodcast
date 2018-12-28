2b50ccb93cfe29a000ef98d0a3275d4fc1381a6b
Daniel Aditya Istyana <adityadaniel@daniels-macbook-pro.local>
2018-12-28 12:59:48 +0700

    Initial Commit

---
+++ LBTAPodcast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LBTAPodcast
+//
+//  Created by Daniel Aditya Istyana on 28/12/18.
+//  Copyright © 2018 Daniel Aditya Istyana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
